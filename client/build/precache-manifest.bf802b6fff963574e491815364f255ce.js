8d8cb95b5967ff161c4427cbf0e757edfa310ab5
Bapuso Sawant <67903457+Bapuso-Sawant@users.noreply.github.com>
2022-09-06 21:39:22 +0530

    Add files via upload

---
+++ client/build/precache-manifest.bf802b6fff963574e491815364f255ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dca2f11e862730a242974c77bda5cde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23c5d6acd0c3484d381d",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "6b236774dbb30443a3ff",
+    "url": "/static/js/2.2b276330.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.2b276330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c5d6acd0c3484d381d",
+    "url": "/static/js/main.d507a05c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.d507a05c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
